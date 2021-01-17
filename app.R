77939876b4cc56d4260530b029c1d96caaa26851
iqbaleffendy <miqbaleffendy18@gmail.com>
2021-01-17 16:48:28 +0700

    Developing first menu which is 'Service Level'.

--- app.R
+++ app.R
@@ -1,23 +1,57 @@
 library(shiny)
+library(shinydashboard)
+library(shinyWidgets)
 library(shinythemes)
+library(tidyverse)
+library(readxl)
+library(DT)
+library(plotly)
+library(formattable)
 
-# Define UI for application that draws a histogram
+
+# Load Dataset----
+service_level <- read_excel("import_data/Service Level.xlsx")
+
+# UI----
 ui <- fluidPage(
+  
   themeSelector(),
+  useShinydashboard(),
+  
   navbarPage(
     title = "PSM Dashboard",
     tabPanel(
-      title = "Menu A",
+      title = "Service Level",
       sidebarLayout(
         sidebarPanel(
-          sliderInput("bins",
-                      "Number of bins:",
-                      min = 1,
-                      max = 50,
-                      value = 30)
+          selectInput(
+            inputId = "customercode",
+            label = "Select Costumer Code",
+            choices = c("All", unique(service_level$Customer)),
+            selected = "All"
+          ),
+          selectInput(
+            inputId = "branchname",
+            label = "Select Branch Name",
+            choices = c("All", unique(service_level$Branch)),
+            selected = "All"
+          ),
+          dateRangeInput(
+            inputId = "dates",
+            label = "Select Date",
+            start = min(service_level$Month),
+            end = max(service_level$Month)
+          ),
+          textOutput("cutoffdate")
         ),
         mainPanel(
-          plotOutput("distPlot")
+          fluidRow(
+            valueBoxOutput("servicelevelpercentage"),
+            valueBoxOutput("servicelevelvalue", width = 5) 
+          ),
+          fluidRow(
+            plotlyOutput("servicelevelplot")
+          )
         )
       )
     ),
@@ -33,14 +67,70 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  output$distPlot <- renderPlot({
-    # generate bins based on input$bins from ui.R
-    x    <- faithful[, 2]
-    bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  # Reactive Expression to Filter Service Level Data----
+  service_level_filtered <- reactive({
+    if (input$customercode != "All") {
+      service_level <- service_level %>% 
+        filter(Customer == input$customercode)
+    }
     
-    # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    if (input$branchname != "All") {
+      service_level <- service_level %>% 
+        filter(Branch == input$branchname)
+    }
+    
+    #service_level <- service_level %>% 
+      #filter(OpenDate >= input$dates[1] & OpenDate <= input$dates[2])
+    
+    service_level
   })
+  
+  # Output Service Level Percentage----
+  output$servicelevelpercentage <- renderValueBox({
+    servicelvlpct <- 
+      service_level_filtered() %>% 
+      summarize(sumprocessed = sum(Processed), sumfilled = sum(Filled)) %>% 
+      ungroup() %>% 
+      mutate(percentage = sumfilled / sumprocessed * 100) %>% 
+      select(percentage)
+    valueBox(
+      paste(round(servicelvlpct, digits = 1), " %"),
+      subtitle = "Service Level Percentage",
+      color = "blue"
+    )
+  })
+  
+  # Output Service Level Value----
+  output$servicelevelvalue <- renderValueBox({
+    servicelvlvalue <- 
+      service_level_filtered() %>% 
+      summarize(totalvalue = sum(Value))
+    valueBox(
+      paste("Rp", accounting(servicelvlvalue, digits = 0L)),
+      subtitle = "Service Level Value",
+      color = "blue"
+    )
+  })
+  
+  # Output Service Level Plot----
+  output$servicelevelplot <- renderPlotly({
+    service_level %>% 
+      group_by(Branch) %>% 
+      summarize(sumprocessed = sum(Processed), sumfilled = sum(Filled)) %>% 
+      ungroup() %>% 
+      mutate(Branch = fct_reorder(Branch, sumprocessed, .desc = TRUE)) %>% 
+      plot_ly(x = ~Branch, y = ~sumprocessed, type = "bar", name = "Processed") %>% 
+      add_trace(y = ~sumfilled, name = "Filled") %>% 
+      layout(yaxis = list(title = 'Count'), barmode = 'group')
+  })
+  
+  # Output Cut Off Date----
+  output$cutoffdate <- renderText({
+    paste(
+      "Cut Off Date is ", as.Date(max(service_level$Month))
+    )
+  })
+  
 }
 
 # Run the application 
